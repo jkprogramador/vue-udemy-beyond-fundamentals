1f8acc06321d00cb926dcd7a059e2a7142a916b9
Jack Lee <br.sp.jkemail@gmail.com>
2022-03-31 15:34:55 -0300

    Using Lifecycle Hooks

--- app.js
+++ app.js
@@ -3,9 +3,42 @@ const vm = Vue.createApp({
         return {
             message: 'Hello World!'
         }
+    },
+    beforeCreate() {
+        // No access to data yet
+        console.log('beforeCreate() function called!', this.message)
+    },
+    created() {
+        // Data has been initialized
+        console.log('created() function called!', this.message)
+    },
+    beforeMount() {
+        // Application does not have access to template yet
+        console.log('beforeMount() function called!', this.$el)
+    },
+    mounted() {
+        // Template has been compiled and inserted into DOM
+        console.log('mounted() function called!', this.$el)
+    },
+    beforeUpdate() {
+        // Whenever an update in data occurs, but before change is applied to template
+        console.log('beforeUpdate() function called!')
+    },
+    updated() {
+        console.log('updated() function called!')
+    },
+    beforeUnmount() {
+        // Data is still available
+        console.log('beforeUnmount() function called!')
+    },
+    unmounted() {
+        // Any directives and events are gone
+        console.log('unmounted() function called!')
     }
 })
 
+vm.mount('#app')
+
 setTimeout(() => {
-    vm.mount('#app')
-}, 3000)
+    vm.unmount()
+}, 5000)
