1f344cc91620d46f6d4d17e7711e8ff5e11ed3d0
Jack Lee <br.sp.jkemail@gmail.com>
2022-03-31 16:21:05 -0300

    The Vue Compiler

--- app.js
+++ app.js
@@ -4,41 +4,7 @@ const vm = Vue.createApp({
             message: 'Hello World!'
         }
     },
-    beforeCreate() {
-        // No access to data yet
-        console.log('beforeCreate() function called!', this.message)
-    },
-    created() {
-        // Data has been initialized
-        console.log('created() function called!', this.message)
-    },
-    beforeMount() {
-        // Application does not have access to template yet
-        console.log('beforeMount() function called!', this.$el)
-    },
-    mounted() {
-        // Template has been compiled and inserted into DOM
-        console.log('mounted() function called!', this.$el)
-    },
-    beforeUpdate() {
-        // Whenever an update in data occurs, but before change is applied to template
-        console.log('beforeUpdate() function called!')
-    },
-    updated() {
-        console.log('updated() function called!')
-    },
-    beforeUnmount() {
-        // Data is still available
-        console.log('beforeUnmount() function called!')
-    },
-    unmounted() {
-        // Any directives and events are gone
-        console.log('unmounted() function called!')
-    }
+    template: `{{ message }}`
 })
 
 vm.mount('#app')
-
-setTimeout(() => {
-    vm.unmount()
-}, 5000)
